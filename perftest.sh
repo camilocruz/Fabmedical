995598008ede720881a387211e9e2d2135dc6660
camilocruz <camilo.cruz@gmail.com>
2022-03-01 19:43:34 +0000

    Added Application Insights

--- perftest.sh
+++ perftest.sh
@@ -1,6 +1,6 @@
-host="fabmedical-[SUFFIX].documents.azure.com"
-username="fabmedical-[SUFFIX]"
-password="[PASSWORD]"
+host="fabmedical-551292.mongo.cosmos.azure.com"
+username="fabmedical-551292"
+password="42Cwgwt6PEvnIZRXRK2vBMjRq4jm8A5DQnVhXKorNrDK5We75oecQuKABEKRZ0dseinHr8z4WiNlUWnQJo5W1w=="
 dbname="contentdb"
 port="10255"
 
